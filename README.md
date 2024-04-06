c35ca8a21e77538ef90a0443c5f744dbf54bd4ca
AdrianTerciado <terciadojano@gmail.com>
2024-04-06 08:31:56 +0200

    Version 1.0.0 finished

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Proyecto-personal-front
+# Aplicación web de películas
+Movies web-App
+
+## El funcionamiento de la aplicación es muy sencillo, al iniciar la app nos muestra las peliculas en cartelera. 
+
+![Pantalla_principal](https://github.com/AdrianTerciado/Movies-webApp/assets/158854133/3ee88d22-8290-422a-b62f-eb46ce757d68)
+
+
+
+
+
+## Pinchando en cada una, podemos acceder a información de la película.
+
+![Info_pelicula](https://github.com/AdrianTerciado/Movies-webApp/assets/158854133/533a9fb2-8e35-4df3-9edb-58e691d8eb6a)
+
+
+
+
+
+## Además tenemos una barra de búsqueda para filtrar por título y ver los resultados.
+
+![Busqueda](https://github.com/AdrianTerciado/Movies-webApp/assets/158854133/363efff8-d0ff-48d7-b3b8-f9c980ed4043)
+
+
+
+
+
+Estoy trabajando para implementar otro tipo de mejoras como poder loguearnos para guardar una lista de películas favoritas.
+
+## Tecnologías utilizadas:
+* HTML
+* CSS
+* JavaScript
+* EM6
+* GitHub
+ - Trabajo de ramas: main, develop y dos ramas de desarrollo (movie_page para pintar la página de infor de películas y Search_movie para la parte del buscador)
+
+
+
+He tratado de cuidar la maquetación para que visualmente sea atractiva, se hace una manipulación del DOM y llamada a API para traer los datos. 
+
+## En un proyecto de front-end que puedes visualizar en:
+https://adrianterciado.github.io/Movies-webApp/
+
+## Si quieres forquear el proyecto y probarlo por tu cuenta, debes obtener una API_KEy en: 
+https://www.themoviedb.org/
+
+
+Échale un vistazo y si crees que puedes colaborar, ¡muchas gracias!
--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-/* BEERER */
+//const KEY="***********" // Aquí debes introducir tu API_KEY  
 
+const IMG = "https://image.tmdb.org/t/p/w500";
 
-
-
-async function webMovie() { // Simplemente recoge los datos de la api para pintar el DOM
-
-    /*    CREDENCIALES */
-
-    const URL = "./movies.json"; // Local
-    const IMG = "https://image.tmdb.org/t/p/w500";
-
-    try {
-        // Lanzamos la solicitud utilizando fetch() y esperamos respuesta
-        const response = await fetch(URL);
-        // Verificamos que la respuesta es correcta, es una buena práctica y pide de comer
-        if (!response.ok) {
-            console.error("Houston, la respuesta no ha sido del todo correcta");
-        }
-        const data = await response.json();
-        // Ya tenemos nuestros datos listos para procesar
-        // Sólo necesito "results", que es un array de objetos con la info de las pelis        
-
-        let moviesInformation = data.results;
-        console.log(moviesInformation); // Sólo para verificar
-
-        let nowPlaying = "";
-        // de momento queiro pintar una tarjeta con su foto y el nomobre
-        moviesInformation.forEach(movie => {
-            nowPlaying += `
-            <article class="containerMovie">
+const cardTemplate = function (movie) {
+    return `<article class="template_movie" data-movie-id="${movie.id}">
                 <figure>
-                    <img src="${IMG+movie.poster_path}" alt="${movie.title}" class = "pictureMovie">
+                    <img src="${IMG + movie.poster_path}" alt="${movie.title}" class="picture_movie">
                 </figure>
                 <p>${movie.title}</p>
             </article>`
+}
 
+async function webMovie() { 
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${KEY}`);
+
+        if (!response.ok) {
+            console.error("Houston, la respuesta no ha sido del todo correcta");
+        }
+        const data = await response.json();       
+
+        let movies = data.results;
+
+        let billboard = `
+            <h1 class="title">Últimos estrenos</h1>
+            <div class="movies_container">`;
+
+        movies.forEach(movie => {
+            billboard += cardTemplate(movie);
         });
-        document.querySelector(".nowPlaying_container").innerHTML = nowPlaying;
 
+        billboard += `</div>`
+
+        document.querySelector(".billboard").innerHTML = billboard;
+
+        document.querySelectorAll(".template_movie").forEach(element => {
+            element.addEventListener("click", function () {
+                const movieId = this.dataset.movieId;
+                printMoviePage(movieId)
+            })
+        })
     }
     catch (error) {
         console.error("Houston, tenemos un problema: " + error)
@@ -45,3 +47,63 @@ async function webMovie() { // Simplemente recoge los datos de la api para pinta
 }
 
 webMovie();
+
+document.querySelector("#search_button").addEventListener("click", async function () {
+
+    let title = "&query=" + document.getElementById("search_movie").value;
+
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}${title}`);
+        if (!response.ok) {
+            console.error("Houston, la respuesta no ha sido del todo correcta");
+            document.querySelector(".result").innerHTML = "No figura en la base de datos";
+        }
+        const data = await response.json();
+
+        let movies = data.results;
+
+        let result = `
+            <h1 class="title">Resultados:</h1>
+            <div class="movies_container">`;
+
+        movies.forEach(movie => {
+            result += cardTemplate(movie);
+        });
+
+        result += "</div>";
+        document.querySelector(".movie-page").innerHTML = "";
+        document.querySelector(".search_result").innerHTML = result;
+
+        document.querySelectorAll(".template_movie").forEach(element => {
+            element.addEventListener("click", function () {
+                const movieId = this.dataset.movieId;
+                printMoviePage(movieId)
+            })
+        })
+    }
+    catch (error) {
+        console.error("Houston, tenemos un problema: " + error)
+        document.querySelector(".search_result").innerHTML = "No figura en la base de datos";
+    }
+})
+
+async function printMoviePage(data) {
+    console.log(data);
+    let response = await fetch(`https://api.themoviedb.org/3/movie/${data}?api_key=${KEY}`)
+    let movie = await response.json();
+    console.log(movie);
+
+    document.querySelector(".search_result").innerHTML = "";
+    document.querySelector(".billboard").innerHTML = "";
+    document.querySelector(".movie-page").innerHTML = `
+        <div>
+            <img src="${IMG + movie.backdrop_path}" alt="${movie.title}">
+        </div>
+        <div>
+            <p>Título: ${movie.title}</p>
+            <p>Sinopsis: ${movie.overview}</p>
+            <p>Web: ${movie.homepage}</p>
+            <p>${movie.tagline}</p>
+            <p>Valoración: ${movie.vote_average}</p>
+        </div>`
+}
